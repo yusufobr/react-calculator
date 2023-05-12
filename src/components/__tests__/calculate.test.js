19eb94589dae1e934444d996cf49b5a23b4579e7
Collins Bawa <collinskwamebawa@yahoo.com>
2023-05-12 17:15:09 +0000

    Simulate user interaction

--- src/components/__tests__/calculate.test.js
+++ src/components/__tests__/calculate.test.js
@@ -1,26 +1,51 @@
+import React from 'react';
+import { render, fireEvent } from '@testing-library/react';
 import calculate from '../../logic/calculate';
+import Calculator from '../Calculator';
 
 describe('calculate', () => {
   test('returns an empty object when buttonName is "AC"', () => {
     const result = calculate({}, 'AC');
-    expect(result).toEqual({ total: null, next: null, operation: null });
+    expect(result).toMatchSnapshot();
   });
 
   test('updates "next" with a number when no operation is set', () => {
     const obj = { next: '5', total: null, operation: null };
     const result = calculate(obj, '2');
-    expect(result).toEqual({ next: '52', total: null });
+    expect(result).toMatchSnapshot();
   });
 
   test('appends a number to "next" when an operation is set', () => {
     const obj = { next: '5', total: null, operation: '+' };
     const result = calculate(obj, '2');
-    expect(result).toEqual({ next: '52', total: null, operation: '+' });
+    expect(result).toMatchSnapshot();
   });
 
   test('performs the operation when "next" and an operation are set', () => {
     const obj = { next: '5', total: '10', operation: 'x' };
     const result = calculate(obj, '=');
-    expect(result).toEqual({ total: '50', next: null, operation: null });
+    expect(result).toMatchSnapshot();
+  });
+});
+
+describe('Calculator Component', () => {
+  test('simulates user interaction', () => {
+    const { getByText } = render(<Calculator />);
+
+    const button1 = getByText('1');
+    fireEvent.click(button1);
+
+    const buttonPlus = getByText('+');
+    fireEvent.click(buttonPlus);
+
+    const button5 = getByText('5');
+    fireEvent.click(button5);
+
+    const buttonEqual = getByText('=');
+    fireEvent.click(buttonEqual);
+
+    const result = getByText('6');
+
+    expect(result).toBeInTheDocument();
   });
 });
--- src/components/__tests__/operate.test.js
+++ src/components/__tests__/operate.test.js
@@ -3,61 +3,60 @@ import Calculator from '../Calculator';
 import operate from '../../logic/operate';
 
 describe('operate', () => {
-describe('add', () => {
-test('should add two numbers correctly', () => {
-expect(operate(2, 2, '+')).toMatchSnapshot();
-expect(operate(5, 5, '+')).toMatchSnapshot();
-});
-});
+  describe('add', () => {
+    test('should add two numbers correctly', () => {
+      expect(operate(2, 2, '+')).toMatchSnapshot();
+      expect(operate(5, 5, '+')).toMatchSnapshot();
+    });
+  });
 
-describe('subtract', () => {
-test('should subtract two numbers correctly', () => {
-expect(operate(5, 3, '-')).toMatchSnapshot();
-expect(operate(1, 1, '-')).toMatchSnapshot();
-});
-});
+  describe('subtract', () => {
+    test('should subtract two numbers correctly', () => {
+      expect(operate(5, 3, '-')).toMatchSnapshot();
+      expect(operate(1, 1, '-')).toMatchSnapshot();
+    });
+  });
 
-describe('multiply', () => {
-test('should multiply two numbers correctly', () => {
-expect(operate(5, 3, 'x')).toMatchSnapshot();
-expect(operate(2, 1, 'x')).toMatchSnapshot();
-});
-});
+  describe('multiply', () => {
+    test('should multiply two numbers correctly', () => {
+      expect(operate(5, 3, 'x')).toMatchSnapshot();
+      expect(operate(2, 1, 'x')).toMatchSnapshot();
+    });
+  });
 
-describe('divide', () => {
-test('should divide two numbers correctly', () => {
-expect(operate(5, 5, '÷')).toMatchSnapshot();
-expect(operate(10, 5, '÷')).toMatchSnapshot();
-expect(operate(5, 0, '÷')).toMatchSnapshot();
-});
-});
+  describe('divide', () => {
+    test('should divide two numbers correctly', () => {
+      expect(operate(5, 5, '÷')).toMatchSnapshot();
+      expect(operate(10, 5, '÷')).toMatchSnapshot();
+      expect(operate(5, 0, '÷')).toMatchSnapshot();
+    });
+  });
 
-describe('modulo', () => {
-test('should find modulo of two positive numbers correctly', () => {
-expect(operate(10, 3, '%')).toMatchSnapshot();
-expect(operate(15, 7, '%')).toMatchSnapshot();
-expect(operate(5, 0, '÷')).toEqual("Can't divide by 0.");
-});
-});
+  describe('modulo', () => {
+    test('should find modulo of two positive numbers correctly', () => {
+      expect(operate(10, 3, '%')).toMatchSnapshot();
+      expect(operate(15, 7, '%')).toMatchSnapshot();
+      expect(operate(5, 0, '÷')).toEqual("Can't divide by 0.");
+    });
+  });
 
-describe('unknown operation', () => {
-test('should throw an error for unknown operation', () => {
-expect(() => operate(5, 3, '$')).toThrow("Unknown operation '$'");
-expect(() => operate(10, 5, '@')).toThrow("Unknown operation '@'");
-});
-});
+  describe('unknown operation', () => {
+    test('should throw an error for unknown operation', () => {
+      expect(() => operate(5, 3, '$')).toThrow("Unknown operation '$'");
+      expect(() => operate(10, 5, '@')).toThrow("Unknown operation '@'");
+    });
+  });
 });
 
 describe('Button component', () => {
   test('should update the state when clicked', () => {
     render(<Calculator />);
-    
+
     const buttonElements = screen.getAllByRole('button');
     const acButton = buttonElements.find((button) => button.value === 'AC');
     fireEvent.click(acButton);
-    
+
     const inputElement = screen.getByRole('textbox');
     expect(inputElement).toHaveValue('0');
   });
-
 });
